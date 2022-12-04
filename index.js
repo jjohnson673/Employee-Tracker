e32c7cb84881468277aff205b861a20e7bea77d7
jjohnson673 <jkjohnson673@gmail.com>
2022-12-04 17:08:52 -0500

    added in main home questions and add dept fucnitionality

--- index.js
+++ index.js
@@ -16,7 +16,70 @@ const db = mysql.createConnection({
 });
 
 //Start: What would you like to do....? //
+function start() {
+    inquirer
+        .prompt ([
+            {
+                type: 'list',
+                name: 'startHome',
+                message: 'What would you like to do?',
+                choices: [
+                    'View All Employees',
+                    'View All Departments',
+                    'View All Employee Roles',
+                    'Add New Department',
+                    'Add New Employee',
+                    'Add New Employee Role',
+                    'Change Current Employee',
+                    'Delete Department',
+                    'Delete Employee',
+                    'Delete Employee Role',
+                    'Exit'
+                ]
 
+            }
+        ])
+        .then(answers => {
+            switch(answers.startHome) {
+                case 'View All Employees' :
+                    viewEmployees();
+                    break;
+                case 'View All Departments' :
+                    viewDepartments();
+                    break;
+                case 'View All Employee Roles' :
+                    viewEmployeeRoles();
+                    break;
+                case 'Add New Department' :
+                    addDepartment();
+                    break;
+                case 'Add New Employee' : 
+                    addEmployee();
+                    break;
+                case 'Add New Employee Role' :
+                    addEmployeeRole();
+                    break;
+                case 'Change Current Employee Role' :
+                    changeEmployeeRole();
+                    break;
+                case 'Delete Department' :
+                    deleteDepartment();
+                    break;
+                case 'Delete Employee' :
+                    deleteEmployee();
+                    break;
+                case 'Delete Employee Role' :
+                    deleteEmployeeRole();
+                    break;
+
+                
+                default:
+                    console.log("Exiting Program");
+                    process.exit();
+
+            };
+        });
+};
 
 
 
@@ -27,6 +90,36 @@ const db = mysql.createConnection({
 //-----------//
 
 //ADD DEPARTMENT
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the new department?',
+            default: () => {},
+            validate: name => {
+                let valid = /^[a-zA-Z0-9 ]{1,30}$/.test(name);
+                if (valid) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        }
+    ]).then((answers) => {
+        insertDept(answers.name);
+    });
+}
+
+function insertDept(newDept) {
+    connection.query('INSERT INTO departments SET ?', new Department(newDept), (err, res) => {
+        if (err) throw err;
+        console.log(`Successfully added ${newDept} to Departments`);
+        init();
+    });
+}
 
 //ADD ROLE
 
